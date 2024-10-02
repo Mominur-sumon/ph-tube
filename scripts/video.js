734de676b333a1ddac7ce9901a4a4d27d71ce055
Md Mominur Rahman <mominur.sumon1@gmail.com>
2024-10-03 00:31:40 +0600

    time string with hour minutes and second format

--- scripts/video.js
+++ scripts/video.js
@@ -1,6 +1,15 @@
+// get time string
+function getTimeString (time){
+    
+    const hours = Math.floor(time / 3600);
+    const reminingSeconds = time % 3600;
+    const minutes = Math.floor(reminingSeconds / 60);
+    const seconds = reminingSeconds % 60;
+    return `${hours} hour ${minutes} minutes ${seconds} seconds ago`;
+}
+
 
 const loadCategories = () => {
-    console.log('Loading categories...');
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
         .then(res => res.json())
         .then(data => {
@@ -8,14 +17,7 @@ const loadCategories = () => {
         })
         .catch(err => console.log(err));
 };
-loadCategories();
 
-// category
-// : 
-// "Music"
-// category_id
-// : 
-// "1001"
 
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById('categories');
@@ -30,3 +32,94 @@ const displayCategories = (categories) => {
         categoryContainer.append(btn);
     });
 };
+
+
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => {
+            displayVideos(data.videos);
+        })
+        .catch(err => console.log(err));
+};
+
+
+const cardDemo = {
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+};
+
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos-container');
+    videos.forEach((video) => {
+        console.log(video);
+        // create a card
+        const card = document.createElement('div');
+        card.classList = 'card card-compact';
+        card.innerHTML = `
+            <figure class="h-[200px] relative">
+                <img
+                class="w-full h-full object-cover"
+                src="${video.thumbnail}"
+                alt="" />
+                ${
+                    video.others.posted_date?.length == 0 ? '' : 
+                    `<span class="absolute right-2 bottom-2 bg-black rounded text-white">
+                    ${getTimeString(video.others.posted_date)}</span>`
+                }
+                
+            </figure>
+            <div class="px-0 py-2 flex gap-2">
+               <div>
+                    <img class="w-10 h-10 rounded-full object-cover"
+                     src="${video.authors[0].profile_picture}" alt="  ">
+                    
+                </div>
+                <div>
+                    <h2 class="font-bold">${video.title}</h2>
+                    <div class="flex items-center gap-2">
+                        <p class="text-gray-400">${video.authors[0].profile_name}</p>
+                        ${video.authors[0].verified ? '<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">' : ''}
+                        
+                    </div>
+                    <p></p>
+                </div>
+                
+            </div>` ;
+
+        // add card to container
+        videoContainer.append(card);
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
+loadCategories();
+loadVideos();
+
+
+
+
